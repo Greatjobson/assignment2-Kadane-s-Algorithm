b6d5c7918ef8563212fda292197539b04181c210
Greatjobson <zumabejba@gmail.com>
2025-10-03 22:07:40 +0500

    init: maven project structure, junit5, ci setup

---
+++ src/main/java/cli/BenchmarkRunner.java
@@ -0,0 +1,8 @@
+package cli;
+/*
+Бэнчмарк сделать чтобы тестировать его производительность (performance) на разных
+размерах данных (n = 100, 1000, 10000, 100000, 1000000),
+как требует раздел Empirical Validation.
+*/
+public class BenchmarkRunner {
+}
